d214bf8c001a50f3adac609e800726453dc7c6ea
alize2e <73970956+alize2e@users.noreply.github.com>
2024-08-16 09:47:31 -0700

    Update GrayRecComp.lean
    Proved that grayRecSlides and genGray have same output, with a lemma for the sort of symmetry of helpGG.

--- GrayRecComp.lean
+++ GrayRecComp.lean
@@ -19,6 +19,10 @@ def Subset.genGray (n : Nat) : List (Subset n) :=
 
 #eval (Subset.genGray 3) == (Subset.grayRecSlides 3)
 
+-- `TODO`
+-- add xor 1100000... and how equivalent to reversing and complementing first? p 284
+-- prove that gray only changes one bit at a time? (or do that with it?) how do i even formalize the theorem? p 283
+
 theorem Subset.helpGRS_parity_reverse {n : Nat} : (helpGRS n false).reverse = helpGRS n true := by
   induction n with
   | zero => rfl
@@ -31,25 +35,30 @@ theorem Subset.helpGRS_parity_reverse {n : Nat} : (helpGRS n false).reverse = he
       _ = ((helpGRS n' false).map (cons true)) ++ ((helpGRS n' true).map (cons false)) := by simp
       _ = helpGRS n'.succ true := by rfl
 
-theorem Subset.helpGG_symmetry {n : Nat} :
+theorem Subset.helpGG_symmetry {n : Nat} {l : List (Subset n)} {soFar : List (Subset (n+1))} :
+  helpGG l soFar = (l.map (cons false)) ++ List.reverseAux (l.map (cons true)) soFar := by
+    induction l generalizing soFar with
+    | nil => rfl
+    | cons x xs ih =>
+      calc helpGG (x::xs) soFar
+        _ = (cons false x) :: helpGG xs ((cons true x) :: soFar) := by rfl
+        _ = (cons false x) :: (xs.map (cons false)) ++ List.reverseAux (xs.map (cons true)) ((cons true x) :: soFar) := by simp [ih]
+        _ = ((x::xs).map (cons false)) ++ List.reverseAux (xs.map (cons true)) ((cons true x) :: soFar) := by rfl
+        _ = ((x::xs).map (cons false)) ++ List.reverseAux ((cons true x)::xs.map (cons true)) soFar := by rfl
+        _ = ((x::xs).map (cons false)) ++ List.reverseAux ((x::xs).map (cons true)) soFar := by rfl
 
--- theorem Subset.genGray_IS {n : Nat} :
---   ((genGray n).map (cons false)) ++ ((genGray n).map (cons true)).reverse = genGray n.succ := by
---     induction n with
---     | zero => rfl
---     | succ n' ih =>
---       calc ((genGray n'.succ).map (cons false)) ++ ((genGray n'.succ).map (cons true)).reverse
---         _ = ((((genGray n').map (cons false)) ++ ((genGray n').map (cons true)).reverse).map (cons false)) ++ ((((genGray n').map (cons false)) ++ ((genGray n').map (cons true)).reverse).map (cons true)).reverse := by simp [ih]
-
--- theorem Subset.gray_rec_eq {n : Nat} : grayRecSlides n = genGray n := by
---   induction n with
---   | zero => rfl
---   | succ n' ih =>
---     calc grayRecSlides n'.succ
---       _ = helpGRS n'.succ false := by rfl
---       _ = ((helpGRS n' false).map (cons false)) ++ ((helpGRS n' true).map (cons ¬false)) := by rfl
---       _ = ((helpGRS n' false).map (cons false)) ++ ((helpGRS n' false).reverse.map (cons ¬false)) := by rw [helpGRS_parity_reverse]
---       _ = ((grayRecSlides n').map (cons false)) ++ ((grayRecSlides n').reverse.map (cons ¬false)) := by rfl
---       _ = ((genGray n').map (cons false)) ++ ((genGray n').reverse.map (cons ¬false)) := by rw [ih]
---       _ = ((genGray n').map (cons false)) ++ ((genGray n').reverse.map (cons true)) := by simp
---       _ = ((genGray n').map (cons false)) ++ ((genGray n').map (cons true)).reverse := by simp [List.reverse_map]
+theorem Subset.gray_rec_eq {n : Nat} : grayRecSlides n = genGray n := by
+  induction n with
+  | zero => rfl
+  | succ n' ih =>
+    calc grayRecSlides n'.succ
+      _ = helpGRS n'.succ false := by rfl
+      _ = ((helpGRS n' false).map (cons false)) ++ ((helpGRS n' true).map (cons ¬false)) := by rfl
+      _ = ((helpGRS n' false).map (cons false)) ++ ((helpGRS n' false).reverse.map (cons ¬false)) := by rw [helpGRS_parity_reverse]
+      _ = ((grayRecSlides n').map (cons false)) ++ ((grayRecSlides n').reverse.map (cons ¬false)) := by rfl
+      _ = ((genGray n').map (cons false)) ++ ((genGray n').reverse.map (cons ¬false)) := by rw [ih]
+      _ = ((genGray n').map (cons false)) ++ ((genGray n').reverse.map (cons true)) := by simp
+      _ = ((genGray n').map (cons false)) ++ ((genGray n').map (cons true)).reverse := by simp [List.reverse_map]
+      _ = ((genGray n').map (cons false)) ++ ((genGray n').map (cons true)).reverseAux [] := by rfl
+      _ = helpGG (genGray n') [] := by rw [helpGG_symmetry]
+      _ = genGray n'.succ := by rfl
