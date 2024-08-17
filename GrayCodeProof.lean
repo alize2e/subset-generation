277f97255afdc8c2e9003b73219d7ead7e1da88c
alize2e <73970956+alize2e@users.noreply.github.com>
2024-08-16 22:38:17 -0700

    Update GrayCodeProof.lean
    Completed i.succ = (grayRecSlides n').length case.

--- GrayCodeProof.lean
+++ GrayCodeProof.lean
@@ -37,13 +37,16 @@ theorem List.diy_get_map (α β : Type) (f : α → β) (l : List α) (i : Nat) 
 theorem List.diy_reverse_zero_last {α : Type} {l : List α} {h : 0<l.length} {h' : l.reverse.length-1<l.reverse.length} : l.reverse[l.reverse.length-1] = l[0] := by
   match l with
   | x::xs =>
-    -- have : (xs.reverse++[x]).length = (x::xs).length := by simp
     have : (x::xs).reverse = xs.reverse++[x] := List.reverse_cons x xs
     have : ¬ (xs.reverse++[x]).length-1 < xs.reverse.length := by simp
     calc (x::xs).reverse[(x::xs).reverse.length-1]
       _ = (xs.reverse++[x])[(xs.reverse++[x]).length-1] := by simp
       _ = x := List.get_last this
       _ = (x::xs)[0] := by simp
+
+-- copied from documentation, idk why not imported
+theorem Nat.sub_one_add_one_eq_of_pos : ∀ {n}, 0 < n → (n - 1) + 1 = n
+  | _+1, _ => rfl
 
 -- edit so that i show it's also a gray cycle by changing h to ≤ not <, but a pain for indices
 theorem Subset.grayRecSlides_one_change_next {n : Nat} {parity : Bool} {i : Nat} {h : i.succ<(grayRecSlides n).length} :
@@ -81,6 +84,16 @@ theorem Subset.grayRecSlides_one_change_next {n : Nat} {parity : Bool} {i : Nat}
           have : ((grayRecSlides n').reverse).length = ((grayRecSlides n').map (cons false)).length := by simp
           have h1 : (((grayRecSlides n').map (cons false)) ++ ((grayRecSlides n').reverse.map (cons true)))[i] = ((grayRecSlides n').map (cons false))[i] := List.get_append_left ((grayRecSlides n').map (cons false)) ((grayRecSlides n').reverse.map (cons true)) h3
           have h5 : (((grayRecSlides n').map (cons false)) ++ ((grayRecSlides n').reverse.map (cons true)))[((grayRecSlides n').map (cons false)).length] = ((grayRecSlides n').reverse.map (cons true))[((grayRecSlides n').map (cons false)).length-((grayRecSlides n').map (cons false)).length] := List.get_append_right ((grayRecSlides n').map (cons false)) ((grayRecSlides n').reverse.map (cons true)) (by simp [hi])
+          have h7 : 0 < (grayRecSlides n').reverse.reverse.length := by
+            calc 0
+              _ < 2^n' := by simp [Nat.two_pow_pos]
+              _ = (grayRecSlides n').length := by rw [grayRecSlides_num]
+              _ = (grayRecSlides n').reverse.reverse.length := by simp
+          have h6 : (grayRecSlides n').reverse.reverse.length - 1 < (grayRecSlides n').reverse.reverse.length := by
+            calc (grayRecSlides n').reverse.reverse.length - 1
+              _ < ((grayRecSlides n').reverse.reverse.length - 1).succ := by simp
+              _ = (grayRecSlides n').reverse.reverse.length - 1 + 1 := by simp
+              _ = (grayRecSlides n').reverse.reverse.length := by rw [Nat.sub_one_add_one_eq_of_pos h7]
           calc num_changes (grayRecSlides n'.succ)[i] (grayRecSlides n'.succ)[i.succ]
             _ = num_changes (((grayRecSlides n').map (cons false)) ++ ((grayRecSlides n').reverse.map (cons true)))[i] (((grayRecSlides n').map (cons false)) ++ ((grayRecSlides n').reverse.map (cons true)))[i.succ] := by simp [hi, grayRecSlides_IS n']
             _ = num_changes ((grayRecSlides n').map (cons false))[i] (((grayRecSlides n').map (cons false)) ++ ((grayRecSlides n').reverse.map (cons true)))[i.succ] := by rw [h1]
@@ -89,8 +102,8 @@ theorem Subset.grayRecSlides_one_change_next {n : Nat} {parity : Bool} {i : Nat}
             _ = num_changes (cons false (grayRecSlides n')[i]) ((grayRecSlides n').reverse.map (cons true))[((grayRecSlides n').map (cons false)).length-((grayRecSlides n').map (cons false)).length] := by rw [h5]
             _ = num_changes (cons false (grayRecSlides n')[i]) ((grayRecSlides n').reverse.map (cons true))[0] := by simp
             _ = num_changes (cons false (grayRecSlides n')[i]) (cons true (grayRecSlides n').reverse[0]) := by rw [List.diy_get_map (Subset n') (Subset n'.succ) (cons true) (grayRecSlides n').reverse 0]
-            -- _ = num_changes (cons false (grayRecSlides n')[i]) (cons true (grayRecSlides n').getLast) := by simp [List.getLast_reverse]
-            _ = num_changes (cons false (grayRecSlides n')[i]) (cons true (grayRecSlides n')[(grayRecSlides n').length-1]) := sorry
+            _ = num_changes (cons false (grayRecSlides n')[i]) (cons true (grayRecSlides n').reverse.reverse[(grayRecSlides n').reverse.reverse.length-1]) := by rw [List.diy_reverse_zero_last]
+            _ = num_changes (cons false (grayRecSlides n')[i]) (cons true (grayRecSlides n')[(grayRecSlides n').length-1]) := by simp
             _ = num_changes (cons false (grayRecSlides n')[i]) (cons true (grayRecSlides n')[i.succ-1]) := by simp [hi]
             _ = num_changes (cons false (grayRecSlides n')[i]) (cons true (grayRecSlides n')[i]) := by simp
             _ = 1 + num_changes (grayRecSlides n')[i] (grayRecSlides n')[i] := by rfl
