55b9168cd31ce31501a348da8a7a1e92d25d8eb5
alize2e <73970956+alize2e@users.noreply.github.com>
2024-08-20 10:28:16 -0700

    Update PlainItEqT.lean
    Cleaned up and edited so that it works with "sorry"s.

--- PlainItEqT.lean
+++ PlainItEqT.lean
@@ -2,11 +2,6 @@
 
 import «Subsets».working.eqTransport
 
--- def Subset.genIt (n : Nat) : List (Subset n) :=
---   let rec help {l r : Nat} (xsl : Subset l) (xsr : Subset r) (soFar : List (Subset n))
---     (sum_l_r_n : l+r=n) (zeros : xsl.binValOne 0 = 0) (nel : soFar.length>0) (lastStart : Subset l)
---     (last_eq : soFar[0] = lastStart.append xsr) : List (Subset n) :=
-
 def Subset.genIt (n : Nat) : List (Subset n) :=
   let rec help {l r l' : Nat} (xsl : Subset l) (xsr : Subset r) (soFar : List (Subset n))
     (sum_l_r_n : l+r=n) (zeros : xsl.binValOne 0 = 0) (nel : soFar.length>0) (lastStart : Subset l')
@@ -29,25 +24,8 @@ def Subset.genIt (n : Nat) : List (Subset n) :=
                 _ = (cons false xsl).binValOne 0 := rfl
                 _ = (if false then 1 else 0) + 2*xsl.binValOne 0 := by simp [binValOne]
                 _ = 0 := by simp [zeros]
-            have l_lt_n : l<n :=
-              calc l
-                _ < l+1 := by simp
-                _ = l+(cons x nil).card := by simp
-                _ ≤ l+((cons x nil).card + xsr'.card) := by simp_arith
-                _ = l+((cons x nil).append xsr').card := by simp [append_card]
-                _ = n := by simp [sum_l_r_n]
-            let lastStart' := append lastStart (cons x nil) --lastStart.append (cons (soFar[0].toFun (l.toFin n l_lt_n)) nil)
+            let lastStart' := append lastStart (cons x nil)
             have eq_t' : eqTransport soFar[0] (append lastStart' xsr') := by simp only [lastStart', ←eqT_append_IS, eq_t]
-            -- have card_last' : (lastStart'.append xsr').card = n :=
-            --   calc (lastStart'.append xsr').card
-            --     _ = lastStart'.card + xsr'.card := by simp [append_card]
-            --     _ = (lastStart.append (cons (soFar[0].toFun (l.toFin n l_lt_n)) nil)).card + xsr'.card := by rfl
-            --     _ = lastStart.card + 1 + xsr'.card := by simp [append_card]
-            --     _ = xsl.card + 1 + xsr'.card := by simp [card_l]
-            --     _ = xsl'.card + xsr'.card := by simp
-            --     _ = n := by simp [sum_l'_r'_n]
-            -- have card_last : n = (lastStart.append (cons x xsr')).card := sorry--by simp_arith [sum_l_r_n, Nat.add_comm, card_last]
-            -- have card_int : ((lastStart.append (cons x nil)).append xsr').card = n := sorry
             have card_l' : xsl'.card = lastStart'.card :=
               calc xsl'.card
                 _ = xsl.card + 1 := by simp
@@ -63,10 +41,11 @@ def Subset.genIt (n : Nat) : List (Subset n) :=
                 _ = (Subset.append xsl (Subset.cons true xsr')).card := by rfl
                 _ = xsl.card + (Subset.cons true xsr').card := by rw [append_card]
                 _ = n := by rw [sum_l_r_n]
-            have sum_l'_r'_n :  Subset.nil.card + xs.card = n := by simp_arith [xs_len_eq_n]
             -- have : xs.binValOne 1 < soFar[0].binValOne 1 := sorry
-            have d2 : (Subset.cast (by simp[xs_len_eq_n]) xs : Subset n).binValOne 1 < soFar[0].binValOne 1 := sorry
-            help Subset.nil (xs) ((Subset.cast (by simp [xs_len_eq_n]) xs) :: soFar) sum_l'_r'_n rfl (by simp [nel]) Subset.nil rfl (by simp [append, eqT_self])
+            let xs' := (Subset.cast (by simp [xs_len_eq_n]) xs)
+            have sum_l'_r'_n :  Subset.nil.card + xs'.card = n := by simp_arith [xs_len_eq_n]
+            have d2 : xs'.binValOne 1 < soFar[0].binValOne 1 := sorry
+            help Subset.nil xs' (xs' :: soFar) sum_l'_r'_n rfl (by simp [nel]) Subset.nil rfl (eqT_self xs')
       termination_by (soFar[0].binValOne 1, binVal xsl xsr 0)
       -- xsl'++xsr' = incr xsl++xsn (where lsb is left)
 
