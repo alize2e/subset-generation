917d9521f5cc2d731d0cc93aa865f915b08a22a6
alize2e <73970956+alize2e@users.noreply.github.com>
2024-08-20 22:27:45 -0700

    Update GrayItProof.lean
    Moved a lot to GrayValProof, proved some stuff but have problems with it not recognizing equivalence or replacement of a with cons a0 as, and not sure how to go about proving stuff about minLeft1? for second part.

--- GrayItProof.lean
+++ GrayItProof.lean
@@ -1,21 +1,6 @@
-import «Subsets».SubsetIsoFun
+import «Subsets».working.GrayValProof
 
-def Subset.grayVal {m : Nat} (times : Nat) : Subset m → Nat × Bool
-  | nil => (0, false)
-  | cons b bs =>
-    match grayVal (2*times) bs with
-    | (val, parity) => (val + times*(if b.xor parity then 0 else 1), b.xor parity)
-
-def Subset.minLeft1? {n : Nat} : Subset n → Option (Subset n)
-  | nil => none
-  | cons true (cons b bs) => some (cons true (cons (!b) bs))
-  | cons b bs =>
-    let out := minLeft1? bs
-    match out with
-    | none => none
-    | some next => some (cons b next)
-
-def Subset.helpGrayIt {m : Nat} (parity : Bool) (soFar : List (Subset m.succ))
+def Subset.helpGrayIt {m : Nat} (parity : Bool) (soFar : List (Subset (m+1)))
   (soFar_len : soFar.length>0) (parity_def : parity = !(soFar[0].grayVal 1).snd) : List (Subset m.succ) :=
     match soFar with
     | a :: xs =>
@@ -31,28 +16,26 @@ def Subset.helpGrayIt {m : Nat} (parity : Bool) (soFar : List (Subset m.succ))
               _ = !(xor a₀ (grayVal 2 as).snd) := by simp [Bool.not_xor]
               _ = !(grayVal 1 (cons a₀ as)).snd := by rfl
               _ = true := by simp [parity_def]
-          have : (grayVal 1 (cons (!a₀) as)).fst < (grayVal 1 a).fst := sorry
+          have : (grayVal 1 (cons a₀ as)).snd = false :=
+            calc (grayVal 1 (cons a₀ as)).snd
+              _ = !!(grayVal 1 (cons a₀ as :: xs)[0]).snd := by simp
+              _ = !true := by rw [parity_def]
+          have : (grayVal 1 (cons (!a₀) as)).fst+1 = (grayVal 1 (cons a₀ as)).fst := dec_case_1 a₀ as this
+          have : (grayVal 1 (cons (!a₀) as)).fst+1 ≤ (grayVal 1 (cons a₀ as)).fst := by simp [this]
+          -- why does it want a and not (cons a₀ as)? how does a even exist?
+          have : (grayVal 1 (cons (!a₀) as)).fst+1 = (grayVal 1 a).fst := sorry
+          have : (grayVal 1 (cons (!a₀) as)).fst+1 ≤ (grayVal 1 a).fst := by simp [this]
           helpGrayIt (!parity) (a' :: soFar) (by simp [soFar_len]) (by simp [parity_def', h])
       | false =>
         let out := minLeft1? a
         match h2 : out with
         | none => soFar
         | some next =>
-          have : (grayVal 1 next).fst < (grayVal 1 a).fst := sorry
+          have : (grayVal 1 next).fst+1 = (grayVal 1 a).fst := sorry
+          have : (grayVal 1 next).fst+1 ≤ (grayVal 1 a).fst := by simp [this]
           have parity_def' : (grayVal 1 next).snd = false := sorry
           helpGrayIt (!parity) (next :: soFar) (by simp [soFar_len]) (by simp [parity_def', h])
       termination_by (soFar[0].grayVal 1).fst
-
-theorem Subset.false_grayVal_false {m : Nat} : ∀ times : Nat, ((initFalse m.succ).grayVal times).snd = false := by
-  induction m with
-  | zero => intro times; rfl
-  | succ m' ih =>
-    intro times
-    calc (grayVal times (initFalse m'.succ.succ)).snd
-      _ = (grayVal times (cons (false) (initFalse m'.succ))).snd := by rfl
-      _ = xor false (grayVal (2*times) (initFalse m'.succ)).snd := by rfl
-      _ = (grayVal (2*times) (initFalse m'.succ)).snd := by simp
-      _ = false := by simp [ih]
 
 def Subset.grayIt (n : Nat) : List (Subset n) :=
   match n with
