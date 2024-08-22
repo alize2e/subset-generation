dca5a6deafbfe7fc4ffc5e043432c1921d901bc2
alize2e <73970956+alize2e@users.noreply.github.com>
2024-08-22 14:29:55 -0700

    Update GrayRecSlides.lean
    Copied some stuff from GrayRecComp to prove grayRecSlides_num

--- GrayRecSlides.lean
+++ GrayRecSlides.lean
@@ -7,6 +7,8 @@ def Subset.helpGRS (n : Nat) (parity : Bool) : List (Subset n) :=
 
 def Subset.grayRecSlides (n : Nat) : List (Subset n) := helpGRS n false
 
+-- `TODO` uniqueness of elements easier to prove here?
+
 #eval Subset.grayRecSlides 3
 
 theorem Subset.in_helpGRS {n : Nat} {s : Subset n} : ∀ parity : Bool, s ∈ helpGRS n parity := by
@@ -31,3 +33,35 @@ theorem Subset.in_helpGRS {n : Nat} {s : Subset n} : ∀ parity : Bool, s ∈ he
 theorem Subset.in_grayRecSlides {n : Nat} {s : Subset n} : s ∈ grayRecSlides n :=
   have : grayRecSlides n = helpGRS n false := by rfl
   by simp [this, in_helpGRS]
+
+theorem Subset.helpGRS_parity_reverse {n : Nat} : (helpGRS n false).reverse = helpGRS n true := by
+  induction n with
+  | zero => rfl
+  | succ n' ih =>
+    calc (helpGRS n'.succ false).reverse
+      _ = (((helpGRS n' false).map (cons false)) ++ ((helpGRS n' true).map (cons true))).reverse := by rfl
+      _ = ((helpGRS n' true).map (cons true)).reverse ++ ((helpGRS n' false).map (cons false)).reverse := by simp
+      _ = ((helpGRS n' true).reverse.map (cons true)) ++ ((helpGRS n' false).reverse.map (cons false)) := by simp [List.reverse_map]
+      _ = ((helpGRS n' false).reverse.reverse.map (cons true)) ++ ((helpGRS n' true).map (cons false)) := by rw [ih]
+      _ = ((helpGRS n' false).map (cons true)) ++ ((helpGRS n' true).map (cons false)) := by simp
+      _ = helpGRS n'.succ true := by rfl
+
+theorem Subset.grayRecSlides_IS (n : Nat) : grayRecSlides n'.succ = ((grayRecSlides n').map (cons false)) ++ ((grayRecSlides n').reverse.map (cons true)) :=
+  calc grayRecSlides n'.succ
+    _ = helpGRS n'.succ false := by rfl
+    _ = ((helpGRS n' false).map (cons false)) ++ ((helpGRS n' true).map (cons ¬false)) := by rfl
+    _ = ((helpGRS n' false).map (cons false)) ++ ((helpGRS n' false).reverse.map (cons ¬false)) := by rw [helpGRS_parity_reverse]
+    _ = ((grayRecSlides n').map (cons false)) ++ ((grayRecSlides n').reverse.map (cons true)) := by rfl
+
+theorem Subset.grayRecSlides_num {n : Nat} : (grayRecSlides n).length = 2^n := by
+  induction n with
+  | zero => rfl
+  | succ n' ih =>
+    calc (grayRecSlides n'.succ).length
+      _ = (((grayRecSlides n').map (cons false)) ++ ((grayRecSlides n').reverse.map (cons true))).length := by rw [grayRecSlides_IS n']
+      _ = ((grayRecSlides n').map (cons false)).length + ((grayRecSlides n').reverse.map (cons true)).length := by simp
+      _ = (grayRecSlides n').length + (grayRecSlides n').reverse.length := by simp
+      _ = (grayRecSlides n').length + (grayRecSlides n').length := by simp
+      _ = 2*(grayRecSlides n').length := by simp_arith
+      _ = 2*2^n' := by rw [ih]
+      _ = 2^n'.succ := by rw [Nat.pow_succ']
