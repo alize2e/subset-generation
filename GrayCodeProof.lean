6702ffb5c020ea1b3478717d10676fa9359319a5
alize2e <73970956+alize2e@users.noreply.github.com>
2024-08-21 23:55:07 -0700

    Update GrayCodeProof.lean
    Removed second sorry!

--- GrayCodeProof.lean
+++ GrayCodeProof.lean
@@ -41,7 +41,21 @@ theorem Subset.grayRecSlides_one_change_next {n : Nat} {i : Nat} {h : i.succ<(gr
           have succ_i_in_right : (((grayRecSlides n').map (cons false)) ++ ((grayRecSlides n').reverse.map (cons true)))[i.succ] = ((grayRecSlides n').reverse.map (cons true))[i.succ-((grayRecSlides n').map (cons false)).length] := List.get_append_right ((grayRecSlides n').map (cons false)) ((grayRecSlides n').reverse.map (cons true)) (by assumption)
           have i_in_right : (((grayRecSlides n').map (cons false)) ++ ((grayRecSlides n').reverse.map (cons true)))[i] = ((grayRecSlides n').reverse.map (cons true))[i-((grayRecSlides n').map (cons false)).length] := List.get_append_right ((grayRecSlides n').map (cons false)) ((grayRecSlides n').reverse.map (cons true)) (by assumption)
           have : List.length (grayRecSlides n') - 1 - (i - List.length (grayRecSlides n')) < List.length (grayRecSlides n') := s1 (s0 n')
-          have : Nat.succ (List.length (grayRecSlides n') - 1 - (Nat.succ i - List.length (grayRecSlides n'))) < List.length (grayRecSlides n') := sorry
+          have h15 : (grayRecSlides n').length < i.succ :=
+            calc (grayRecSlides n').length
+              _ = ((grayRecSlides n').map (cons false)).length := by repeat simp
+              _ < i.succ := h4
+          have h16 : i.succ < 2*(grayRecSlides n').length :=
+            calc i.succ
+              _ < (((grayRecSlides n').map (cons false)) ++ ((grayRecSlides n').reverse.map (cons true))).length := by assumption
+              _ = ((grayRecSlides n').map (cons false)).length + ((grayRecSlides n').reverse.map (cons true)).length := by simp
+              _ = (grayRecSlides n').length + (grayRecSlides n').length := by simp
+              _ = 2*(grayRecSlides n').length := by rw [←Nat.two_mul (grayRecSlides n').length]
+          have : Nat.succ (List.length (grayRecSlides n') - 1 - (Nat.succ i - List.length (grayRecSlides n'))) < List.length (grayRecSlides n') :=
+            calc Nat.succ (List.length (grayRecSlides n') - 1 - (Nat.succ i - List.length (grayRecSlides n')))
+              _ < Nat.succ (List.length (grayRecSlides n') - 1) := Nat.succ_lt_succ (s2 h15 (s2' h16 h15))
+              _ = Nat.succ (Nat.pred (List.length (grayRecSlides n'))) := by rfl
+              _ = List.length (grayRecSlides n') := Nat.succ_pred_eq_of_ne_zero (Nat.ne_of_lt' (s0 n'))
           calc num_changes (grayRecSlides n'.succ)[i] (grayRecSlides n'.succ)[i.succ]
             _ = num_changes (((grayRecSlides n').map (cons false)) ++ ((grayRecSlides n').reverse.map (cons true)))[i] (((grayRecSlides n').map (cons false)) ++ ((grayRecSlides n').reverse.map (cons true)))[i.succ] := by simp [grayRecSlides_IS n']
             _ = num_changes ((grayRecSlides n').reverse.map (cons true))[i-((grayRecSlides n').map (cons false)).length] ((grayRecSlides n').reverse.map (cons true))[i.succ-((grayRecSlides n').map (cons false)).length] := by simp [i_in_right, succ_i_in_right]
