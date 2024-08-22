117d0af01333bee8e40f907819862d8654b28476
alize2e <73970956+alize2e@users.noreply.github.com>
2024-08-22 14:46:44 -0700

    Update PlainItSorry.lean

--- PlainItSorry.lean
+++ PlainItSorry.lean
@@ -1,6 +1,6 @@
 -- usable with sorry for d2
 
-import «Subsets».SubsetBinVal
+import «Subsets».BinVal
 
 def Subset.genIt (n : Nat) : List (Subset n) :=
   let rec help {l r : Nat} (xsl : Subset l) (xsr : Subset r) (soFar : List (Subset n))
