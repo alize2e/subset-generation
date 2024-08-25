8b58dd7e21881c8f17c73acf286d81ca3f738d10
alize2e <73970956+alize2e@users.noreply.github.com>
2024-08-24 23:59:48 -0700

    Update GrayItValProof.lean
    Simplified grayVal so it doesn't depend on "times" and its recursive structure is more obvious.

--- GrayItValProof.lean
+++ GrayItValProof.lean
@@ -1,34 +1,33 @@
 import «Subsets».SubsetDef
 
-def Subset.grayVal {m : Nat} (times : Nat) : Subset m → Nat × Bool
+def Subset.grayVal {m : Nat} : Subset m → Nat × Bool
   | nil => (0, false)
   | cons b bs =>
-    match grayVal (2*times) bs with
-    | (val, parity) => (val + times*(if b.xor parity then 0 else 1), b.xor parity)
+    match grayVal bs with
+    | (val, parity) => (2*val + (if b.xor parity then 0 else 1), b.xor parity)
 
-theorem Subset.false_grayVal_false {m : Nat} : ∀ times : Nat, ((initFalse m.succ).grayVal times).snd = false := by
+theorem Subset.false_grayVal_false {m : Nat} : ((initFalse m.succ).grayVal).snd = false := by
   induction m with
-  | zero => intro times; rfl
+  | zero => rfl
   | succ m' ih =>
-    intro times
-    calc (grayVal times (initFalse m'.succ.succ)).snd
-      _ = (grayVal times (cons (false) (initFalse m'.succ))).snd := by rfl
-      _ = xor false (grayVal (2*times) (initFalse m'.succ)).snd := by rfl
-      _ = (grayVal (2*times) (initFalse m'.succ)).snd := by simp
+    calc (grayVal (initFalse m'.succ.succ)).snd
+      _ = (grayVal (cons (false) (initFalse m'.succ))).snd := by rfl
+      _ = xor false (grayVal (initFalse m'.succ)).snd := by rfl
+      _ = (grayVal (initFalse m'.succ)).snd := by simp
       _ = false := by simp [ih]
 
-theorem Subset.dec_case_1 {m : Nat} (a₀ : Bool) (as : Subset m) (h : (grayVal 1 (cons a₀ as)).snd = false) :
-  (grayVal 1 (cons (!a₀) as)).fst+1 = (grayVal 1 (cons a₀ as)).fst :=
+theorem Subset.dec_case_1 {m : Nat} (a₀ : Bool) (as : Subset m) (h : (grayVal (cons a₀ as)).snd = false) :
+  (grayVal (cons (!a₀) as)).fst+1 = (grayVal (cons a₀ as)).fst :=
     match a₀ with
     | true =>
-      if h' : (grayVal 2 as).snd then
+      if h' : (grayVal as).snd then
         by simp [grayVal, h', Bool.xor_false]
       else
-        have : (grayVal 1 (cons true as)).snd = true := by simp [grayVal, h']
+        have : (grayVal (cons true as)).snd = true := by simp [grayVal, h']
         absurd this (by simp [h])
     | false =>
-      if h' : (grayVal 2 as).snd then
-        have : (grayVal 1 (cons false as)).snd = true := by simp [grayVal, h', Bool.xor_false]
+      if h' : (grayVal as).snd then
+        have : (grayVal (cons false as)).snd = true := by simp [grayVal, h', Bool.xor_false]
         absurd this (by simp [h])
       else
         by simp [grayVal, h', Bool.xor_false]
@@ -43,3 +42,21 @@ def Subset.minLeft1? {n : Nat} : Subset n → Option (Subset n)
     | some next => some (cons b next)
 
 -- theorem Subset.dec_case_2 {m : Nat} {a₀ : Bool} {as : Subset m} {h : (grayVal 1 (cons a₀ as)).snd = false} :
+  -- (grayVal next).fst+1 = (grayVal (cons a₀ as)).fst\
+
+def Subset.ml1?_prop {n : Nat} (s : Subset n) : Bool :=
+  match minLeft1? s with
+  | none => true
+  | some out => xor (grayVal out).snd (grayVal s).snd
+
+-- theorem Subset.minLeft1?_change {n : Nat} {s : Subset n} : ml1?_prop s :=
+--   match (minLeft1? s) with
+--   | none => by skip
+--   | some out =>
+
+#eval (Subset.genRec 3)
+#eval (Subset.genRec 3).map Subset.minLeft1?
+
+def s := Subset.cons false (Subset.cons true (Subset.cons true Subset.nil))
+#eval s
+#eval Subset.minLeft1? s
