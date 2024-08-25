f3b4b3b667d8b2912daa616a1bb00156d00b08e5
alize2e <73970956+alize2e@users.noreply.github.com>
2024-08-25 00:00:21 -0700

    Update GrayItProof.lean
    In light of grayVal update.

--- GrayItProof.lean
+++ GrayItProof.lean
@@ -1,7 +1,7 @@
 import «Subsets».GrayItValProof
 
 def Subset.helpGrayIt {m : Nat} (parity : Bool) (soFar : List (Subset (m+1)))
-  (soFar_len : soFar.length>0) (parity_def : parity = !(soFar[0].grayVal 1).snd) : List (Subset m.succ) :=
+  (soFar_len : soFar.length>0) (parity_def : parity = !(soFar[0].grayVal).snd) : List (Subset m.succ) :=
     match soFar with
     | a :: xs =>
       match a with
@@ -9,42 +9,42 @@ def Subset.helpGrayIt {m : Nat} (parity : Bool) (soFar : List (Subset (m+1)))
         match h : parity with
         | true =>
           let a' := cons (!a₀) as
-          have parity_def' : (grayVal 1 a').snd = true :=
-            calc (grayVal 1 a').snd
-              _ = (grayVal 1 (cons (!a₀) as)).snd := by rfl
-              _ = xor (!a₀) (grayVal 2 as).snd := by rfl
-              _ = !(xor a₀ (grayVal 2 as).snd) := by simp [Bool.not_xor]
-              _ = !(grayVal 1 (cons a₀ as)).snd := by rfl
-              _ = true := by simp [parity_def]
-          have : (grayVal 1 (cons a₀ as)).snd = false :=
-            calc (grayVal 1 (cons a₀ as)).snd
-              _ = !!(grayVal 1 (cons a₀ as :: xs)[0]).snd := by simp
+          have parity_def' : (grayVal a').snd = true :=
+            calc (grayVal a').snd
+              _ = xor (!a₀) (grayVal as).snd := by rfl
+              _ = !(xor a₀ (grayVal as).snd) := by simp only [Bool.not_xor]
+              _ = !(grayVal (cons a₀ as)).snd := by rfl
+              _ = true := by simp only [parity_def, List.cons_getElem_zero]
+          have : (grayVal (cons a₀ as)).snd = false :=
+            calc (grayVal (cons a₀ as)).snd
+              _ = !!(grayVal (cons a₀ as :: xs)[0]).snd := by simp only [List.cons_getElem_zero, Bool.not_not]
               _ = !true := by rw [parity_def]
-          have : (grayVal 1 (cons (!a₀) as)).fst+1 = (grayVal 1 (cons a₀ as)).fst := dec_case_1 a₀ as this
-          have : (grayVal 1 (cons (!a₀) as)).fst+1 ≤ (grayVal 1 (cons a₀ as)).fst := by simp [this]
-          helpGrayIt (!parity) (a' :: soFar) (by simp [soFar_len]) (by simp [parity_def', h])
+          have : (grayVal (cons (!a₀) as)).fst+1 = (grayVal (cons a₀ as)).fst := dec_case_1 a₀ as this
+          have : (grayVal (cons (!a₀) as)).fst+1 ≤ (grayVal (cons a₀ as)).fst := by simp only [this, Nat.le_refl]
+          helpGrayIt (!parity) (a' :: soFar) (by simp only [List.length_cons, gt_iff_lt, Nat.zero_lt_succ]) (by simp only [h, Bool.not_true, List.cons_getElem_zero, parity_def'])
         | false =>
           let out := minLeft1? (cons a₀ as)
           match h2 : out with
           | none => soFar
           | some next =>
-            have : (grayVal 1 next).fst+1 = (grayVal 1 (cons a₀ as)).fst := sorry
-            have : (grayVal 1 next).fst+1 ≤ (grayVal 1 (cons a₀ as)).fst := by simp [this]
-            have parity_def' : (grayVal 1 next).snd = false := sorry
-            helpGrayIt (!parity) (next :: soFar) (by simp [soFar_len]) (by simp [parity_def', h])
-      termination_by (soFar[0].grayVal 1).fst
+            have : (grayVal next).fst+1 = (grayVal (cons a₀ as)).fst := sorry
+            have : (grayVal next).fst+1 ≤ (grayVal (cons a₀ as)).fst := by simp only [this, Nat.le_refl]
+            have parity_def' : (grayVal next).snd = false := sorry
+            helpGrayIt (!parity) (next :: soFar) (by simp only [List.length_cons, gt_iff_lt, Nat.zero_lt_succ]) (by simp only [h, Bool.not_false, List.cons_getElem_zero, parity_def'])
+      termination_by (soFar[0].grayVal).fst
 
 def Subset.grayIt (n : Nat) : List (Subset n) :=
   match n with
   | 0 => [nil]
   | m+1 =>
     let init := initFalse m.succ
-    have : !(grayVal 1 init).snd :=
-      calc !(grayVal 1 init).snd
-        _ = !(grayVal 1 (initFalse m.succ)).snd := by rfl
+    have : !(grayVal init).snd :=
+      calc !(grayVal init).snd
+        _ = !(grayVal (initFalse m.succ)).snd := by rfl
         _ = !(false) := by simp [false_grayVal_false]
         _ = true := by simp
     helpGrayIt true [init] (by simp) (by simp [this])
 
 #eval Subset.grayIt 3
-#eval List.map (Subset.grayVal 1) (Subset.grayIt 3)
+#eval List.map (Subset.grayVal) (Subset.grayIt 3)
+#eval List.map (Subset.grayVal) (Subset.grayIt 3)
