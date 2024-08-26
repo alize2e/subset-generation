2062baecb083bc16c53d7cfc1ceb7dd6de3e364f
alize2e <73970956+alize2e@users.noreply.github.com>
2024-08-26 14:25:38 -0700

    Update GrayItProof.lean
    v4.10.0

--- GrayItProof.lean
+++ GrayItProof.lean
@@ -14,14 +14,14 @@ def Subset.helpGrayIt {m : Nat} (parity : Bool) (soFar : List (Subset (m+1)))
               _ = xor (!a₀) (grayVal as).snd := by rfl
               _ = !(xor a₀ (grayVal as).snd) := by simp only [Bool.not_xor]
               _ = !(grayVal (cons a₀ as)).snd := by rfl
-              _ = true := by simp only [parity_def, List.cons_getElem_zero]
+              _ = true := by simp only [parity_def, List.getElem_cons_zero]
           have : (grayVal (cons a₀ as)).snd = false :=
             calc (grayVal (cons a₀ as)).snd
-              _ = !!(grayVal (cons a₀ as :: xs)[0]).snd := by simp only [List.cons_getElem_zero, Bool.not_not]
+              _ = !!(grayVal (cons a₀ as :: xs)[0]).snd := by simp only [List.getElem_cons_zero, Bool.not_not]
               _ = !true := by rw [parity_def]
           have : (grayVal (cons (!a₀) as)).fst+1 = (grayVal (cons a₀ as)).fst := dec_case_1 a₀ as this
           have : (grayVal (cons (!a₀) as)).fst+1 ≤ (grayVal (cons a₀ as)).fst := by simp only [this, Nat.le_refl]
-          helpGrayIt (!parity) (a' :: soFar) (by simp only [List.length_cons, gt_iff_lt, Nat.zero_lt_succ]) (by simp only [h, Bool.not_true, List.cons_getElem_zero, parity_def'])
+          helpGrayIt (!parity) (a' :: soFar) (by simp only [List.length_cons, gt_iff_lt, Nat.zero_lt_succ]) (by simp only [h, Bool.not_true, List.getElem_cons_zero, parity_def'])
         | false =>
           let out := minLeft1? (cons a₀ as)
           match h2 : out with
@@ -30,7 +30,7 @@ def Subset.helpGrayIt {m : Nat} (parity : Bool) (soFar : List (Subset (m+1)))
             have : (grayVal next).fst+1 = (grayVal (cons a₀ as)).fst := sorry
             have : (grayVal next).fst+1 ≤ (grayVal (cons a₀ as)).fst := by simp only [this, Nat.le_refl]
             have parity_def' : (grayVal next).snd = false := sorry
-            helpGrayIt (!parity) (next :: soFar) (by simp only [List.length_cons, gt_iff_lt, Nat.zero_lt_succ]) (by simp only [h, Bool.not_false, List.cons_getElem_zero, parity_def'])
+            helpGrayIt (!parity) (next :: soFar) (by simp only [List.length_cons, gt_iff_lt, Nat.zero_lt_succ]) (by simp only [h, Bool.not_false, List.getElem_cons_zero, parity_def'])
       termination_by (soFar[0].grayVal).fst
 
 def Subset.grayIt (n : Nat) : List (Subset n) :=
