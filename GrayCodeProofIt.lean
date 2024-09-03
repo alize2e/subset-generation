c97ce3d8ac3dc8b5ff17193f74c3f4535a92d508
alize2e <73970956+alize2e@users.noreply.github.com>
2024-09-03 12:06:58 -0700

    Update GrayCodeProofIt.lean
    Finished and simplified proof.

--- GrayCodeProofIt.lean
+++ GrayCodeProofIt.lean
@@ -1,4 +1,4 @@
-import Subsets.GrayIt
+import Subsets.GrayItValProof
 
 theorem Subset.num_changes_change1_eq_1 {n : Nat} {s : Subset n} {m : Nat} {h : m<n} : num_changes (s.change1 m h) s = 1 := by
   induction s generalizing m with
@@ -21,9 +21,8 @@ theorem Subset.num_changes_change1_eq_1 {n : Nat} {s : Subset n} {m : Nat} {h : 
         _ = num_changes (bs.change1 m' this) bs := cons_same_num_changes
         _ = 1 := ih
 
--- figure out how to output proof - make a type of isGray lists?
 def Subset.helpGrayIt_proof {m : Nat} (soFar : List (Subset (m+1))) (_ : soFar.length>0)
-  (proof : isGray soFar) : (List (Subset m.succ)) :=
+  (proof : isGray soFar) : {l : (List (Subset m.succ)) // isGray l} :=
     match soFar with
     | a :: xs =>
       match a with
@@ -37,7 +36,8 @@ def Subset.helpGrayIt_proof {m : Nat} (soFar : List (Subset (m+1))) (_ : soFar.l
           helpGrayIt_proof ((cons (!a₀) as) :: (cons a₀ as) :: xs) (by simp only [List.length_cons, gt_iff_lt, Nat.zero_lt_succ]) proof'
         | true =>
           match h2 : (cons a₀ as).findMinLeft1?.isSome with
-          | false => soFar
+          | false =>
+            ⟨(cons a₀ as :: xs), proof⟩
           | true =>
             let next := (cons a₀ as).change1 ((cons a₀ as).findMinLeft1?.get h2) (by simp)
             have : (grayVal next).fst+1 = (grayVal (cons a₀ as)).fst := dec_case_2 h h2
@@ -47,22 +47,16 @@ def Subset.helpGrayIt_proof {m : Nat} (soFar : List (Subset (m+1))) (_ : soFar.l
             helpGrayIt_proof (next :: (cons a₀ as) :: xs) (by simp only [List.length_cons, gt_iff_lt, Nat.zero_lt_succ]) proof'
       termination_by (soFar[0].grayVal).fst
 
--- theorem Subset.grayIt_one_change_next {n : Nat} {i : Nat} {h₀ : i.succ<(grayIt n).length} {m : Nat} {h₁ : m<n}:
---   num_changes (grayIt n)[i] (grayIt n)[i+1] = 1 := by
---     simp [grayIt]
---     match n with
---     | 0 => nofun
---     | n'+1 =>
---       simp only
---       have h1 : i<(grayIt n'.succ).length := Nat.lt_of_succ_lt h₀
---       match h2 : (grayIt n'.succ)[i].grayVal.snd with
---       | false =>
---         match h3 : (helpGrayIt [initFalse (n' + 1)] grayIt.proof_2)[i] with
---         | cons a₀ as =>
---           -- have : (helpGrayIt [initFalse (n' + 1)] grayIt.proof_2)[i + 1] = cons (!a₀) as := by
+def Subset.grayIt_proof (n : Nat) : {l : (List (Subset n)) // isGray l} :=
+  match n with
+  | 0 => ⟨[nil], rfl⟩
+  | m+1 =>
+    let init := initFalse m.succ
+    have : !(grayVal init).snd :=
+      calc !(grayVal init).snd
+        _ = !(grayVal (initFalse m.succ)).snd := by rfl
+        _ = !(false) := by simp [false_grayVal_false]
+        _ = true := by simp
+    helpGrayIt_proof [init] (by simp [this]) rfl
 
---         -- calc (helpGrayIt [initFalse (n' + 1)] grayIt.proof_2)[i].num_changes (helpGrayIt [initFalse (n' + 1)] grayIt.proof_2)[i + 1]
---         --   _ = (helpGrayIt [initFalse (n' + 1)] grayIt.proof_2)[i].num_changes (helpGrayIt [initFalse (n' + 1)] grayIt.proof_2)[i + 1]
---       | true =>
---       -- (helpGrayIt [initFalse (n' + 1)] grayIt.proof_2)[i].num_changes
---       -- (helpGrayIt [initFalse (n' + 1)] grayIt.proof_2)[i + 1]
+theorem Subset.grayItProof {n : Nat} : isGray (grayIt_proof n).1 := (grayIt_proof n).2
