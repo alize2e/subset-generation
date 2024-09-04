62cddba15a4f34fb09c846b1b54ad487282a940a
alize2e <73970956+alize2e@users.noreply.github.com>
2024-09-04 11:58:14 -0700

    Update ConvertGrayPlainIt.lean
    Changed imports and copied stuff so as not to rely on "sorry" for grayIt termination.

--- ConvertGrayPlainIt.lean
+++ ConvertGrayPlainIt.lean
@@ -1,48 +1,108 @@
-import Subsets.PlainItSorry
+import Subsets.GrayIt
 
-def Subset.grayVal {m : Nat} (times : Nat) : Subset m → Nat × Bool
-  | nil => (0, false)
-  | cons b bs =>
-    match grayVal (2*times) bs with
-    | (val, parity) =>
-      let delta := if b.xor parity then 0 else 1
-      (val + times*delta, b.xor parity)
+def Subset.binVal {l r : Nat} (xsl : Subset l) (xsr : Subset r) (opp : Fin 2) : Nat :=
+  match xsl, xsr with
+  | cons l xsl', xsr => (if l then 1-opp else opp) + 2*(Subset.binVal xsl' xsr opp)
+  | nil, cons r xsr' => (if r then 1-opp else opp) + 2*(Subset.binVal nil xsr' opp)
+  | nil, nil => 0
 
-def Subset.minLeft1? {n : Nat} : Subset n → Option (Subset n)
-  | nil => none
-  | cons true (cons b bs) => some (cons true (cons (¬b) bs))
-  | cons b bs =>
-    let out := minLeft1? bs
-    match out with
-    | none => none
-    | some next => some (cons b next)
+def Subset.binValOne {n : Nat} (s : Subset n) (opp : Fin 2) : Nat :=
+  match s with
+  | nil => 0
+  | cons b s' => (if b then 1-opp else opp) + 2*(Subset.binValOne s' opp)
 
-def Subset.helpGrayIt {m : Nat} (parity : Bool) (soFar : List (Subset m.succ))
-  (ok : soFar.length>0) : List (Subset m.succ) :=
-    match soFar with
-    | a :: xs =>
-      match parity with
-      | true =>
-        match a with
-        | cons a₀ as =>
-          let a' := cons (¬ a₀) as
-          have : (grayVal 1 (cons (!a₀) as)).fst < (grayVal 1 (cons a₀ as)).fst := sorry
-          helpGrayIt (¬ parity) (a' :: soFar) (by simp [ok])
-      | false =>
-        let out := minLeft1? a
-        match out with
-        | none => soFar
-        | some next =>
-          have : (grayVal 1 next).fst < (grayVal 1 a).fst := sorry
-          helpGrayIt (¬ parity) (next :: soFar) (by simp [ok])
-      termination_by (soFar[0].grayVal 1).fst
+theorem Subset.binVal_append {l r : Nat} {sl : Subset l} {sr : Subset r} {opp : Fin 2} :
+  Subset.binVal sl sr opp = (Subset.append sl sr).binValOne opp := by
+    induction sl with
+    | nil =>
+      induction sr with
+      | nil => simp only [binVal, binValOne]
+      | cons r sr' ihr =>
+        calc Subset.binVal Subset.nil (Subset.cons r sr') opp
+          _ = (if r then 1-opp else opp) + 2*(Subset.binVal Subset.nil sr' opp) := by simp only [binVal]
+          _ = (if r then 1-opp else opp) + (2*(Subset.append Subset.nil sr').binValOne opp) := by rw [ihr]
+    | cons l sl' ihl =>
+      calc Subset.binVal (Subset.cons l sl') sr opp
+        _ = (if l then 1-opp else opp) + 2*(Subset.binVal sl' sr opp) := by simp [Subset.binVal]
+        _ = (if l then 1-opp else opp) + 2*(Subset.append sl' sr).binValOne opp := by rw [ihl]
 
-def Subset.grayIt (n : Nat) : List (Subset n) :=
-  match n with
-  | 0 => [nil]
-  | m+1 =>
-    let init := initFalse m.succ
-    helpGrayIt true [init] (by simp)
+theorem Subset.binVal_power {l r : Nat} {xsl : Subset l} {xsr : Subset r} {opp : Fin 2} :
+  Subset.binVal xsl xsr opp = (xsl.binValOne opp) + 2^xsl.card*(xsr.binValOne opp) := by
+    induction xsl with
+    | nil =>
+      calc Subset.binVal Subset.nil xsr opp
+        _ = (Subset.append Subset.nil xsr).binValOne opp := by rw [binVal_append]
+        _ = xsr.binValOne opp := by rfl
+        _ = 0 + 2^0*(xsr.binValOne opp) := by simp_arith
+        _ = (Subset.nil.binValOne opp) + 2^0*(xsr.binValOne opp) := by rfl
+    | cons l xsl' ih =>
+      calc Subset.binVal (Subset.cons l xsl') xsr opp
+        _ = (if l then 1-opp else opp) + 2*(Subset.binVal xsl' xsr opp) := by simp [Subset.binVal]
+        _ = (if l then 1-opp else opp) + 2*(xsl'.binValOne opp + 2^(xsl'.card)*(xsr.binValOne opp)) := by rw [ih]
+        _ = (if l then 1-opp else opp) + 2*xsl'.binValOne opp + 2*(2^(xsl'.card)*(xsr.binValOne opp)) := by simp_arith [Nat.left_distrib]
+        _ = (if l then 1-opp else opp) + 2*xsl'.binValOne opp + (2^(xsl'.card)*2)*(xsr.binValOne opp) := by simp_arith [Nat.mul_assoc, Nat.mul_comm]
+        _ = (if l then 1-opp else opp) + 2*xsl'.binValOne opp + 2^(xsl'.card+1)*(xsr.binValOne opp) := by simp_arith [Nat.pow_succ]
+        _ = (Subset.cons l xsl').binValOne opp + 2^(xsl'.card+1)*xsr.binValOne opp := by rfl
+
+theorem Subset.dec1 {l r' : Nat} (xsl : Subset l) (x : Bool) (xsr' : Subset r')
+  (zeros : xsl.binValOne 0 = 0) (h : x=true) (xsl' : Subset (l+1)) (xsl'_def : xsl' = cons false xsl):
+    binVal xsl' xsr' 0 < binVal xsl (cons x xsr') 0 :=
+      calc binVal xsl' xsr' 0
+        _ = binVal (cons false xsl) xsr' 0 := by rw [xsl'_def]
+        _ = (if false then 1-0 else 0) + 2*(binVal xsl xsr' 0) := by simp [binVal]
+        _ = 2*(binVal xsl xsr' 0) := by simp
+        _ = 2*((xsl.binValOne 0) + 2^xsl.card*(xsr'.binValOne 0)) := by rw [binVal_power]
+        _ = 2*(2^xsl.card*(xsr'.binValOne 0)) + 0 := by simp_arith [zeros]
+        _ = (2^xsl.card*2)*(xsr'.binValOne 0) := by simp_arith [Nat.mul_assoc, Nat.mul_comm]
+        _ = (2^xsl.card)*(2*(xsr'.binValOne 0)) + 0 := by simp_arith [Nat.mul_assoc]
+        _ < (2^xsl.card)*(2*(xsr'.binValOne 0)) + 1 := by simp_arith
+        _ ≤ (2^xsl.card)*(2*(xsr'.binValOne 0)) + 2^xsl.card := by simp_arith [Nat.one_le_two_pow]
+        _ = 2^xsl.card*((2*xsr'.binValOne 0) + 1) := by rw [Nat.mul_succ]
+        _ = 2^xsl.card*((2*xsr'.binValOne 0) + (if true then 1-0 else 0 : Fin 2)) := by rfl
+        _ = 2^xsl.card*((2*xsr'.binValOne 0) + (if x then 1-0 else 0 : Fin 2)) := by rw [h]
+        _ = 2^xsl.card*((if x then 1-0 else 0 : Fin 2) + 2*(xsr'.binValOne 0)) := by simp_arith [Nat.add_comm]
+        _ = 2^xsl.card*((cons x xsr').binValOne 0) := by simp [binValOne]
+        _ = (xsl.binValOne 0) + 2^xsl.card*((cons x xsr').binValOne 0) := by simp_arith [zeros]
+        _ = binVal xsl (cons x xsr') 0 := by simp [binVal_power]
+
+def Subset.genIt (n : Nat) : List (Subset n) :=
+  let rec help {l r : Nat} (xsl : Subset l) (xsr : Subset r) (soFar : List (Subset n))
+    (sum_l_r_n : l+r=n) (zeros : xsl.binValOne 0 = 0) (nel : soFar.length>0) : List (Subset n) :=
+      match xsr with
+      | Subset.nil => soFar
+      | Subset.cons x xsr' =>
+          if h:x then -- xsl is all 0s
+            let xsl' := Subset.cons false xsl
+            have sum_l'_r'_n : xsl'.card + xsr'.card = n := by
+              calc xsl'.card + xsr'.card
+                _ = xsl.card + 1 + xsr'.card := by rfl
+                _ = xsl.card + xsr'.card + 1 := by simp_arith
+                _ = xsl.card + (Subset.cons x xsr').card := by rfl
+                _ = n := by rw [sum_l_r_n]
+            have d1 : binVal xsl' xsr' 0 < binVal xsl (cons x xsr') 0 :=
+              by simp [dec1 xsl x xsr' zeros h xsl']
+            have zeros' : xsl'.binValOne 0 = 0 :=
+              calc xsl'.binValOne 0
+                _ = (cons false xsl).binValOne 0 := rfl
+                _ = (if false then 1 else 0) + 2*xsl.binValOne 0 := by simp [binValOne]
+                _ = 0 := by simp [zeros]
+            help xsl' xsr' soFar sum_l'_r'_n zeros' nel
+          else
+            let xs := Subset.append xsl (Subset.cons true xsr')
+            have xs_len_eq_n : xs.card = n :=
+              calc xs.card
+                _ = (Subset.append xsl (Subset.cons true xsr')).card := by rfl
+                _ = xsl.card + (Subset.cons true xsr').card := by rw [append_card]
+                _ = n := by rw [sum_l_r_n]
+            have sum_l'_r'_n :  Subset.nil.card + xs.card = n := by simp_arith [xs_len_eq_n]
+            -- have : xs.binValOne 1 < soFar[0].binValOne 1 := sorry
+            have d2 : (Subset.cast (by simp[xs_len_eq_n]) xs : Subset n).binValOne 1 < soFar[0].binValOne 1 := sorry
+            help Subset.nil (xs) ((Subset.cast (by simp [xs_len_eq_n]) xs) :: soFar) sum_l'_r'_n rfl (by simp [nel])
+      termination_by (soFar[0].binValOne 1, binVal xsl xsr 0)
+      -- xsl'++xsr' = incr xsl++xsn (where lsb is left)
+
+  let init := initFalse n
+  help Subset.nil init ([init]) (by simp_arith) rfl (by simp)
 
 def Subset.start' {n : Nat} (g : Subset n) : Bool :=
   match g with
@@ -87,6 +147,3 @@ theorem Subset.φ_ψ_id {n : Nat} {plain : Subset n} : (φ∘ψ) plain = plain :
       _ = cons (xor b (xor p'.start' p'.start')) p' := by rw [Bool.xor_assoc]
       _ = cons (xor b false) p' := by rw [Bool.xor_self]
       _ = cons b p' := by rw [Bool.xor_false]
-
--- `TODO`
--- prove first paragraph of p 284 to prov below?
